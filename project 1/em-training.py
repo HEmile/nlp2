5c9af8365b5c7f755c50f701b95785142347a9d1
Noxeus <ruben@rubenblom.eu>
2017-04-13 11:34:56 +0200

    Structuur.

--- project 1/em-training.py
+++ project 1/em-training.py
@@ -2,17 +2,7 @@ from collections import Counter
 import itertools
 import math
 import numpy as np
-english = []
-french = []
-with open('training/hansards.36.2.e', 'r', encoding='utf8') as f:
-    for l in f:
-        sent = l.split()
-        english.append(sent)
 
-with open('training/hansards.36.2.f', 'r', encoding='utf8') as f:
-    for l in f:
-        sent = l.split()
-        french.append(sent)
 
 def convert_to_ids(sentences):
     ids = {}
@@ -32,17 +22,13 @@ def convert_to_ids(sentences):
         new_sentences.append(sent)
     return new_sentences, id + 1
 
-english, E_vocab_size = convert_to_ids(english)
-french, F_vocab_size = convert_to_ids(french)
-
-print(E_vocab_size)
-print(F_vocab_size)
 
 def pos_alignments(l, m):
     return itertools.product(range(l), repeat=m)
 
 
-# Using log likelihood as described here https://courses.engr.illinois.edu/cs498jh/HW/HW4.pdf
+# Using log likelihood as described here
+# https://courses.engr.illinois.edu/cs498jh/HW/HW4.pdf
 def likelihood(english, french, t):
     sum = 0
     for k in range(len(english)):
@@ -57,26 +43,56 @@ def likelihood(english, french, t):
         sum += sent_sum
     return sum
 
-t = np.full((F_vocab_size, E_vocab_size + 1), 1/F_vocab_size)
+# Set up the data
+def init_data():
+    english = []
+    french = []
+    with open('training/hansards.36.2.e', encoding='utf8') as f:
+        for l in f:
+            sent = l.split()
+            english.append(sent)
 
-for s in range(4):
-    print(s)
-    print(likelihood(english, french, t))
-    align_pairs = Counter()
-    tot_align = Counter()
-    for k in range(len(english)):
-        fdata = french[k]
-        edata = english[k]
-        for i in range(len(fdata)):
-            f = fdata[i]
-            norm = 0
-            for j in range(len(edata)):
-                norm += t[f, edata[j]]
-            for j in range(len(edata)):
-                e = edata[j]
-                delta = t[f, e] / norm
-                align_pairs[e, f] += delta
-                tot_align[e] += delta
-    for f in range(F_vocab_size):
-        for e in range(E_vocab_size):
-            t[f, e] = align_pairs[e, f] / tot_align[e]
+    with open('training/hansards.36.2.f', encoding='utf8') as f:
+        for l in f:
+            sent = l.split()
+            french.append(sent)
+
+    english, E_vocab_size = convert_to_ids(english)
+    french, F_vocab_size = convert_to_ids(french)
+    return (english, french, F_vocab_size, E_vocab_size)
+
+
+def main():
+    english, french, F_vocab_size, E_vocab_size = init_data()
+    print(E_vocab_size)
+    print(F_vocab_size)
+
+    # Init t uniformly
+    t = np.full((F_vocab_size, E_vocab_size + 1), 1/F_vocab_size)
+
+    # Train using EM
+    for s in range(4):
+        print(s)
+        print(likelihood(english, french, t))
+        align_pairs = Counter()
+        tot_align = Counter()
+        for k in range(len(english)):
+            fdata = french[k]
+            edata = english[k]
+            for i in range(len(fdata)):
+                f = fdata[i]
+                norm = 0
+                for j in range(len(edata)):
+                    norm += t[f, edata[j]]
+                for j in range(len(edata)):
+                    e = edata[j]
+                    delta = t[f, e] / norm
+                    align_pairs[e, f] += delta
+                    tot_align[e] += delta
+        for f in range(F_vocab_size):
+            for e in range(E_vocab_size):
+                t[f, e] = align_pairs[e, f] / tot_align[e]
+
+
+if __name__ == '__main__':
+    main()
