f039d37ede045a7f47ddad4f29c8e5b97e480240
Noxeus <ruben@rubenblom.eu>
2017-04-19 09:47:58 +0200

    Code cleaning

--- project 1/em-training.py
+++ project 1/em-training.py
@@ -117,6 +117,7 @@ def em_iteration(english, french, t):
         t[f, e] = align_pairs[e, f] / tot_align[e]
     return t
 
+
 def main():
     english, french, E_vocab_size, F_vocab_size = init_data()
     english, french = english[0], french[0]
@@ -144,7 +145,7 @@ def main():
 def aer_metric():
     english, french, E_vocab_size, F_vocab_size = init_data()
     train_english, train_french = english[0], french[0]
-    
+
     val_english, val_french = english[1], french[1]
 
     # Init t uniformly
@@ -154,7 +155,7 @@ def aer_metric():
 
     for s in range(4):
         t = em_iteration(train_english, train_french, t)
-        
+
         predictions = []
         for k in range(len(val_french)):
             english = val_english[k]
@@ -163,21 +164,21 @@ def aer_metric():
             for i in french:
                 old_val = 0
                 for j in english:
-                    if (i,j) in t.keys():
-                        value = t[(i,j)]
+                    if (i, j) in t.keys():
+                        value = t[(i, j)]
                         if value >= old_val:
-                            best = (i,j)
+                            best = (i, j)
                             old_val = value
                     else:
-                        value = t[(i,'NULL')]
+                        value = t[(i, 'NULL')]
                         if value >= old_val:
-                            best = (i,'NULL')
+                            best = (i, 'NULL')
                             old_val = value
                 sen.add(best)
             predictions.append(sen)
-            
+
         metric = aer.AERSufficientStatistics()
-        # then we iterate over the corpus 
+        # then we iterate over the corpus
         for gold, pred in zip(gold_sets, predictions):
             metric.update(sure=gold[0], probable=gold[1], predicted=pred)
         # AER
@@ -185,5 +186,5 @@ def aer_metric():
 
 
 if __name__ == '__main__':
-    #main()
+    # main()
     aer_metric()
