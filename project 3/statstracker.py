5b329bc314d3738cbfe0455761584a06c8ff0903
Ruben Blom <mail@rubenblom.eu>
2017-06-05 22:16:34 +0200

    plt.figure(0)         plt.plot(self.n, self.loss)

--- project 3/statstracker.py
+++ project 3/statstracker.py
@@ -6,12 +6,14 @@ class StatsTracker:
         self.loss = []
         self.acc = []
         self.lr = []
+        self.plot()
 
     def update(self,n, loss, acc, lr):
         self.n.append(n)
         self.loss.append(loss)
         self.acc.append(acc)
         self.lr.append(lr)
+        self.update_plot()
 
     def plot(self):
         plt.figure(1)
@@ -32,3 +34,13 @@ class StatsTracker:
         plt.plot(self.n, self.lr)
         plt.xlabel('Batch number')
         plt.ylabel('Learning rate')
+
+    def update_plot(self):
+        plt.figure(1)
+        plt.plot(self.n, self.loss)
+
+        plt.figure(2)
+        plt.plot(self.n, self.loss)
+
+        plt.figure(3)
+        plt.plot(self.n, self.loss)
